3e61dccc94a0aa479e83c1618af5044d1090c861
Kamyab Rostami <kamyabrostamii@gmail.com>
2022-07-11 00:20:59 +0430

    Add-Controller

---
+++ RestSamples.ControllerSample/Controllers/MyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestSamples.ControllerSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyController : ControllerBase
+    {
+        public string GetName()
+        {
+            return "Hi";
+        }
+    }
+}
